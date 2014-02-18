ca7047bf4cce0206975c6559f2088cab0577da9b
Hannah <hannah_wight@hotmail.com>
2014-02-18 18:04:47 +0000

    Added cohorts to student list

--- directory.rb
+++ directory.rb
@@ -1,28 +1,28 @@
 #first, we list the names of the students in an array
 students = [
-"Mario Gintili",
-"Mikhail Dubov",
-"Karolis Noreika",
-"Michael Sidon",
-"Charles De Barros",
-"Ruslan Vikhor",
-"Toby Retallick",
-"Mark Mekhaiel",
-"Sarah Young",
-"Hannah Wight",
-"Khushkaran Singh",
-"Rick Brunstedt",
-"Manjit Singh",
-"Alex Gaudiosi",
-"Ross Hepburn",
-"Natascia Marchese",
-"Tiffanie Chia",
-"Matthew Thomas",
-"Freddy McGroarty",
-"Tyler Rollins",
-"Richard Curteis",
-"Anna Yanova",
-"Mystery Student",
+	["Mario Gintili", :february],
+	["Mikhail Dubov", :february],
+	["Karolis Noreika", :february],
+	["Michael Sidon", :february],
+	["Charles De Barros", :february],
+	["Ruslan Vikhor", :february],
+	["Toby Retallick", :february],
+	["Mark Mekhaiel", :february],
+	["Sarah Young", :february],
+	["Hannah Wight", :february],
+	["Khushkaran Singh", :february],
+	["Rick Brunstedt", :february],
+	["Manjit Singh", :february],
+	["Alex Gaudiosi", :february],
+	["Ross Hepburn", :february],
+	["Natascia Marchese", :february],
+	["Tiffanie Chia", :february],
+	["Matthew Thomas", :february],
+	["Freddy McGroarty", :february],
+	["Tyler Rollins", :february],
+	["Richard Curteis", :february],
+	["Anna Yanova", :february],
+	["Mystery Student", :february]
 ]
 
 def print_header
@@ -30,9 +30,9 @@ def print_header
 	puts "------------------------"
 end
 
-def print(names)
-	names.each do |name|
-	puts name
+def print(students)
+	students.each do |student|
+		puts "#{student[0]} (#{student[1]} cohort)"
 	end
 end
 
