e3b38923926c1e7f227d414284926c179a44b297
Hannah <hannah_wight@hotmail.com>
2014-02-18 17:55:28 +0000

    Methods added for clarity

--- directory.rb
+++ directory.rb
@@ -24,11 +24,23 @@ students = [
 "Anna Yanova",
 "Mystery Student",
 ]
-#and now we print them
-puts "The Students of my Cohort at Makers Academy"
-puts "------------------------"
-students.each do |student|
-	puts student
+
+def print_header
+	puts "The Students of my Cohort at Makers Academy"
+	puts "------------------------"
 end
-#finally, we print the total
-puts "Overall, we have #{students.length} great students!"
+
+def print(names)
+	names.each do |name|
+	puts name
+	end
+end
+
+def print_footer(names)
+	puts "Overall, we have #{names.length} great students"
+end
+
+#apparently, nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
