4d23b942bff27288d6351dcc2581ce659cabf040
Hannah <hannah_wight@hotmail.com>
2014-02-21 12:36:55 +0000

    No major updates, just testing things out

--- directory.rb
+++ directory.rb
@@ -1,28 +1,28 @@
 #first, we list the names of the students in an array
 students = [
-	{:name => "Mario Gintili", :cohort => :february, :country_of_birth =>},
-	{:name => "Mikhail Dubov", :cohort => :february, :country_of_birth =>},
-	{:name => "Karolis Noreika", :cohort => :february, :country_of_birth =>},
-	{:name => "Michael Sidon", :cohort => :february, :country_of_birth =>},
-	{:name => "Charles De Barros", :cohort => :february, :country_of_birth =>},
-	{:name => "Ruslan Vikhor", :cohort => :february, :country_of_birth =>},
-	{:name => "Toby Retallick", :cohort => :february, :country_of_birth =>},
-	{:name => "Mark Mekhaiel", :cohort => :february, :country_of_birth =>},
-	{:name => "Sarah Young", :cohort => :february, :country_of_birth =>},
-	{:name => "Hannah Wight", :cohort => :february, :country_of_birth =>},
-	{:name => "Khushkaran Singh", :cohort => :february, :country_of_birth =>},
-	{:name => "Rick Brunstedt", :cohort => :february, :country_of_birth =>},
-	{:name => "Manjit Singh", :cohort => :february, :country_of_birth =>},
-	{:name => "Alex Gaudiosi", :cohort => :february, :country_of_birth =>},
-	{:name => "Ross Hepburn", :cohort => :february, :country_of_birth =>},
-	{:name => "Natascia Marchese", :cohort => :february, :country_of_birth =>},
-	{:name => "Tiffanie Chia", :cohort => :february, :country_of_birth =>},
-	{:name => "Matthew Thomas", :cohort => :february, :country_of_birth =>},
-	{:name => "Freddy McGroarty", :cohort => :february, :country_of_birth =>},
-	{:name => "Tyler Rollins", :cohort => :february, :country_of_birth =>},
-	{:name => "Richard Curteis", :cohort => :february, :country_of_birth =>},
-	{:name => "Anna Yanova", :cohort => :february, :country_of_birth =>},
-	{:name => "Andrew Cumine", :cohort => :february, :country_of_birth =>}
+	{:name => "Mario Gintili", :cohort => :february},
+	{:name => "Mikhail Dubov", :cohort => :february},
+	{:name => "Karolis Noreika", :cohort => :february},
+	{:name => "Michael Sidon", :cohort => :february},
+	{:name => "Charles De Barros", :cohort => :february},
+	{:name => "Ruslan Vikhor", :cohort => :february},
+	{:name => "Toby Retallick", :cohort => :february},
+	{:name => "Mark Mekhaiel", :cohort => :february},
+	{:name => "Sarah Young", :cohort => :february},
+	{:name => "Hannah Wight", :cohort => :february},
+	{:name => "Khushkaran Singh", :cohort => :february},
+	{:name => "Rick Brunstedt", :cohort => :february},
+	{:name => "Manjit Singh", :cohort => :february},
+	{:name => "Alex Gaudiosi", :cohort => :february},
+	{:name => "Ross Hepburn", :cohort => :february},
+	{:name => "Natascia Marchese", :cohort => :february},
+	{:name => "Tiffanie Chia", :cohort => :february},
+	{:name => "Matthew Thomas", :cohort => :february},
+	{:name => "Freddy McGroarty", :cohort => :february},
+	{:name => "Tyler Rollins", :cohort => :february},
+	{:name => "Richard Curteis", :cohort => :february},
+	{:name => "Anna Yanova", :cohort => :february},
+	{:name => "Andrew Cumine", :cohort => :february}
 ]
 
 def print_header
@@ -31,8 +31,8 @@ def print_header
 end
 
 def print(students)
-	students.each do |student|
-		puts "#{student[:name]} (#{student[:cohort]} cohort) from #{:country_of_birth}"
+	students.each_with_index do |student|
+		puts " #{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
 
@@ -40,27 +40,10 @@ def print_footer(names)
 	puts "Overall, we have #{names.length} students"
 end
 
-def input_students
-	puts "Please enter the name of the students"
-	puts "To finish, just hit return twice"
-	#create an empty array
-	students = [ ]
-	#get the first name
-	name = gets.chomp
-	#while the name is not empty, repeat this code
-	while !name.empty? do
-		#add the student hash to the array
-		students << {:name => name, :cohort => :november}
-		puts "Now we have #{students.length} students"
-		#get another name from the user
-		name = gets.chomp
-	end
-	#return the array of students
-	students
-end
+
+
 
 #apparently, nothing happens until we call the methods
-students = input_students
 print_header
 print(students)
 print_footer(students)
