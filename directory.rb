1f667e72dada8ee9f4b3ebea379041370f027a37
Hannah <hannah_wight@hotmail.com>
2014-02-24 19:09:48 +0000

    Updated directory - exercises 1-3

--- directory.rb
+++ directory.rb
@@ -31,19 +31,54 @@ def print_header
 end
 
 def print(students)
-	students.each_with_index do |student|
-		puts " #{student[:name]} (#{student[:cohort]} cohort)"
+	students.each_with_index do |student, i|
+		puts "#{1+i} #{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
+
+
+def select_students_starting_with(letter, students)
+	students.select{|student| student[:name][0] ==  letter}
+end
+
+
 
 def print_footer(names)
 	puts "Overall, we have #{names.length} students"
 end
 
 
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	# create an empty array
+	students = []
+	# get the first name
+	name = gets.chomp
+	#while the name is not empty, repeat this code
+	while !name.empty? do
+		puts "Enter the cohort"
+		cohort = gets.chomp
+		students << {:name => name, :cohort => :february} # this is called a shovel method.
+		# array.<<() also works
+		puts "Now we have #{students.length} students"
+		#then get another name from the user
+		name = gets.chomp
+	end
+	#return the array of students
+	students
+end
+
+# could do something with the cohort?  Like cohort ||= "Unknown Cohort"?
+
+# \n = newline
+# name = gets, enter name, gets returned with a newline at the end.
+# chomp gets rid of the newline.
+# chomp! modifies the original, so it doesn't ever come back with the newline
 
 
 #apparently, nothing happens until we call the methods
 print_header
 print(students)
+# print(select_students_starting_with('A', students))
 print_footer(students)
